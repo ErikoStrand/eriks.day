bbbd9c5fe29cf9436c7d63fc9b67e1acde32b10e
Erik Strandberg <erikstrandberg153@gmail.com>
2025-05-12 14:31:22 +0200

    routes for contact, about, blog.
    i want it to feel handcrafted with nice small animations and a clean design.
    
    i think i want a transition between the pages, not sure what tho.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="flex min-h-screen flex-col items-center justify-center p-4 text-white">
+<div class="flex min-h-svh flex-col items-center justify-center p-4 text-white">
 	<h1 class="mb-4 text-center text-4xl font-bold text-stone-800">
 		Hello, welcome to eriks<span class="text-blue-500">.day</span>
 	</h1>
