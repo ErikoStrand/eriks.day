d941c0cd03ec0824454d567bc476ebea1705e493
Erik Strandberg <erikstrandberg153@gmail.com>
2025-05-12 14:09:54 +0200

    first nav

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+	let links = {
+		'/contact': 'Contact',
+		'/about': 'About me',
+		'/blog': 'Blog'
+	};
+	let visible = $state(false);
+	onMount(() => {
+		visible = true;
+	});
+</script>
+
+<nav class="flex flex-row justify-between px-2 py-2 md:px-8">
+	<a href="/" class=" font-mono text-xl">eriks<span class="text-blue-500">.day</span></a>
+	<section class="flex flex-row gap-4 text-xl font-semibold">
+		{#each Object.entries(links) as [link, name], i}
+			{#if visible}
+				<a
+					in:fly={{ x: 50, duration: 500, delay: 50 * i }}
+					href={link}
+					class="text-stone-800 hover:text-blue-500"
+				>
+					{name}
+				</a>
+			{/if}
+		{/each}
+	</section>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 </script>
 
-<div class="flex min-h-screen flex-col items-center justify-center bg-gray-900 p-4 text-white">
-	<h1 class="mb-4 text-center text-4xl font-bold">
+<div class="flex min-h-screen flex-col items-center justify-center p-4 text-white">
+	<h1 class="mb-4 text-center text-4xl font-bold text-stone-800">
 		Hello, welcome to eriks<span class="text-blue-500">.day</span>
 	</h1>
 </div>
