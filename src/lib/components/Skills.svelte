5af6276f015277ac809b87ff75c1ad32d5a29efc
Erik Strandberg <erikstrandberg153@gmail.com>
2025-05-20 14:43:32 +0200

    proficiences.

---
+++ src/lib/components/Skills.svelte
@@ -0,0 +1,69 @@
+<script>
+	let proficiences = [
+		{
+			name: 'html',
+			level: 80
+		},
+		{
+			name: 'css',
+			level: 68
+		},
+		{
+			name: 'jaavascript',
+			level: 76
+		},
+		{
+			name: 'svelte',
+			level: 78
+		},
+		{
+			name: 'php',
+			level: 34
+		},
+		{
+			name: 'nodejs',
+			level: 35
+		},
+		{
+			name: 'sveltekit',
+			level: 82
+		},
+		{
+			name: 'typescript',
+			level: 23
+		},
+		{
+			name: 'python',
+			level: 69
+		},
+		{
+			name: 'sql',
+			level: 56
+		},
+		{
+			name: 'unity',
+			level: 36
+		},
+		{
+			name: 'problem solving',
+			level: 88
+		},
+		{
+			name: 'teamwork',
+			level: 76
+		},
+		{
+			name: 'communication',
+			level: 87
+		},
+		{
+			name: 'leadership',
+			level: 90
+		},
+		{
+			name: 'creativity',
+			level: 89,
+			explaination: "I have grand ideas but I havn't unlocked the ability to execute them yet."
+		}
+	];
+</script>
